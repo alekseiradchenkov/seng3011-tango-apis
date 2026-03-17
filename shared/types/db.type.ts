6aa23b38c513a25ff5e98cacd78d99ea646a466e
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2026-03-17 14:47:45 +1100

    implemented Charts to database

--- shared/types/db.type.ts
+++ shared/types/db.type.ts
@@ -1,4 +1,5 @@
 import { AdageEvent, AdageTimeObject, AnyValue } from "./adage.type";
+import { Chart } from "./chart.type";
 
 export type DatasetMetadata = {
   data_source: string;
@@ -11,15 +12,29 @@ export type DatasetMetadata = {
   filters?: DatasetFilters;
 };
 
+export type ChartMetadata = {
+  chart_id: string;
+  user_id: string;
+  time_object: AdageTimeObject;
+  filters?: ChartFilters;
+}
+
 export type DatasetStore = {
   dataset_id: string;
   events: AdageEvent[];
 };
 
 export type DatasetFilters = Record<string, AnyValue>;
+export type ChartFilters = Record<string, AnyValue>;
 
 /* user: UserDatasetMetadata object...*/
 export type DatasetMetadataTable = Record<string, DatasetMetadata[]>;
 
 /* dataset_id: DataStore object... */
 export type DatasetStores = Record<string, DatasetStore>;
+
+/* user: UserChartMetadata object... */
+export type ChartMetadataTable = Record<string, ChartMetadata[]>;
+
+/* chart_id: Chart object... */
+export type ChartStores = Record<string, Chart>
